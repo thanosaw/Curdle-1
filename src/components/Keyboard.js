d8bdc2d5a2c5583a753f8e2a7d5c83300baadfcd
thanosaw <56616222+thanosaw@users.noreply.github.com>
2022-05-01 19:03:24 -0700

    Update Keyboard.js

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,9 +1,74 @@
 // download dszna (ES7+ extension to use rfce to make auto import functions)
-import React from 'react'
-
+import React, {useContext, useCallback, useEffect} from 'react';
+import {AppContext} from '../App';
+import Key from "./Key"
 function Keyboard() {
+  const { onEnter, onDelete, onSelectLetter} = useContext(AppContext);
+  //Each row of the keyboard is represented as an array
+  const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+  const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+  const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];//Each line for each row of the keyboard
+
+
+
+
+  const handleKeyboard = useCallback((event) =>  {
+    if (event.key === "Enter" ){
+      onEnter()
+    }
+    else if(event.key ==="Backspace"){
+      onDelete();
+    }
+    else{
+      keys1.forEach((key) => {
+        if (event.key.toLowerCase()===key.toLowerCase()){
+          onSelectLetter(key)
+        }
+      })
+      keys2.forEach((key) => {
+        if (event.key.toLowerCase()===key.toLowerCase()){
+          onSelectLetter(key)
+        }
+      })
+      keys3.forEach((key) => {
+        if (event.key.toLowerCase()===key.toLowerCase()){
+          onSelectLetter(key)
+        }
+      })
+    }
+  })
+
+
+
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyboard);
+    
+    return () => {
+      document.removeEventListener("keydown", handleKeyboard);
+
+    };
+  }, [handleKeyboard])
   return (
-    <div>Keyboard</div>
+    <div className="keyboard" onKeyDown= {handleKeyboard}>
+      <div className = "line1">
+        {keys1.map((key) => {
+        return <Key keyVal ={key} />;
+      })}
+      </div>
+      <div className = "line2">
+      {keys2.map((key) => {
+        return <Key keyVal ={key}/>; 
+      })}
+      </div>
+      <div className = "line3">
+        <Key keyVal = {"ENTER"} bigKey/>
+      {keys3.map((key) => {
+        return <Key keyVal ={key}/>;
+      })}
+      <Key keyVal = {"DELETE"} bigKey/>
+      </div>
+    </div>
   )
 }
 
