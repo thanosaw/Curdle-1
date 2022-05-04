734b9d0cead557c2911b9900870920ed49073807
leoonnnnn <39069236+leoonnnnn@users.noreply.github.com>
2022-05-03 18:50:16 -0700

    Colored Letters

--- src/Words.js
+++ src/Words.js
@@ -6,5 +6,5 @@ export const boardDefault = [
     ["", "", "", "", ""],
     ["", "", "", "", ""],
     ["", "", "", "", ""],
-    ];
+];
     // array for board has empty arrays for when the game starts for 6 attempts
--- src/components/Letter.js
+++ src/components/Letter.js
@@ -2,13 +2,17 @@
 import React, {useContext} from 'react';
 import {AppContext} from '../App';
 
-function Letter({letterPos, attemptVal}) 
-{
-const{board} = useContext(AppContext);
-const letter = board[attemptVal][letterPos];
-// accesses each individual letter
-return <div className = "letter"> {letter}</div>;
-// shows letter text
+function Letter({letterPos, attemptVal}) {
+  const{board, correctWord, currAttempt} = useContext(AppContext);
+  const letter = board[attemptVal][letterPos];
+  // accesses each individual letter
+
+  const correct = correctWord[letterPos] === letter;
+  const almost = !correct && letter !== "" && correctWord.includes(letter);
+
+  const letterState = currAttempt.attempt > attemptVal && (correct ? "correct" : almost ? "almost" : "error");
+  return <div className = "letter" id={letterState}> {letter}</div>;
+  // shows letter text
 }
 
 export default Letter;
