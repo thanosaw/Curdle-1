5799384c2f49e49d11bb9a3281ce5e2b02a8c875
jormbee <86024490+jormbee@users.noreply.github.com>
2022-05-04 14:50:14 -0700

    Getting words from Word Bank commit

--- src/Words.js
+++ src/Words.js
@@ -1,3 +1,5 @@
+import wordBank from './wordle-bank.txt'
+
 /* Words to be used*/
 export const boardDefault = [
     ["", "", "", "", ""],
@@ -8,3 +10,14 @@ export const boardDefault = [
     ["", "", "", "", ""],
 ];
     // array for board has empty arrays for when the game starts for 6 attempts
+
+export const generateWordSet = async () => {
+    let wordSet;
+    await fetch(wordBank)
+    .then((response) => response.text())
+    .then((result) => {
+        const wordArr = result.split("\n")
+        wordSet = new Set(wordArr);
+    });
+    return{ wordSet };
+}
